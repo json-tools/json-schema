b8e4a16f71befe7fbf8edc61d4efcc5e7e277170
Anatoliy Chakkaev <mail@anatoliy.in>
2017-08-12 16:32:24 +0100

    Test validations

---
+++ tests/Validation.elm
@@ -0,0 +1,46 @@
+module Validation exposing (all)
+
+--import Json.Schema.Builder exposing (buildSchema)
+import Data.Schema exposing (blankSchema)
+import Json.Schema exposing (validate)
+import Json.Encode as Encode
+import Test exposing (Test, describe, test)
+import Expect
+
+all : Test
+all = describe "validations"
+    [ describe "multipleOf"
+        [ test "success with int" <|
+            \() ->
+                { blankSchema | multipleOf = Just 2 }
+                    |> validate (Encode.int 4)
+                    |> Expect.equal (Ok True)
+        , test "success with float" <|
+            \() ->
+                { blankSchema | multipleOf = Just 2.1 }
+                    |> validate (Encode.float 4.2)
+                    |> Expect.equal (Ok True)
+        , test "success with periodic float" <|
+            \() ->
+                { blankSchema | multipleOf = Just (1/3) }
+                    |> validate (Encode.float (2/3))
+                    |> Expect.equal (Ok True)
+        , test "failure" <|
+            \() ->
+                { blankSchema | multipleOf = Just 3 }
+                    |> validate (Encode.float (2/7))
+                    |> Expect.equal (Err "Value is not the multiple of 3")
+        ]
+    , describe "maximum"
+        [ test "success" <|
+            \() ->
+                { blankSchema | maximum = Just 2 }
+                    |> validate (Encode.int 2)
+                    |> Expect.equal (Ok True)
+        , test "failure" <|
+            \() ->
+                { blankSchema | maximum = Just 2 }
+                    |> validate (Encode.float 2.1)
+                    |> Expect.equal (Err "Value is above the maximum of 2")
+        ]
+    ]
