3e1d482e8b25db9b5dbba8d0ce8113f8e073e346
Anatoliy <ac@ub.io>
2017-08-15 17:44:04 +0100

    Added type stringifyer to helpers

---
+++ src/Json/Schema/Helpers.elm
@@ -0,0 +1,55 @@
+module Json.Schema.Helpers exposing (typeToString)
+
+import Data.Schema
+    exposing
+        ( Type(AnyType, SingleType, NullableType, UnionType)
+        , SingleType(IntegerType, NumberType, StringType, BooleanType, NullType, ArrayType, ObjectType)
+        )
+
+
+singleTypeToString : SingleType -> String
+singleTypeToString st =
+    case st of
+        StringType ->
+            "string"
+
+        IntegerType ->
+            "integer"
+
+        NumberType ->
+            "float"
+
+        BooleanType ->
+            "boolean"
+
+        ObjectType ->
+            "object"
+
+        ArrayType ->
+            "array"
+
+        NullType ->
+            "null"
+
+
+typeToString : Type -> String
+typeToString t =
+    case t of
+        NullableType NullType ->
+            "null"
+
+        NullableType st ->
+            "nullable " ++ (singleTypeToString st)
+
+        SingleType s ->
+            singleTypeToString s
+
+        UnionType l ->
+            l
+                |> List.map singleTypeToString
+                |> String.join ", "
+
+        AnyType ->
+            "any"
+
+
